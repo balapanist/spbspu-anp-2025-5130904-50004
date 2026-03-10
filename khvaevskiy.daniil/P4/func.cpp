e53149ac0251d4181a0592c3ee672b4fbc5354c8
balapanist <fefedge10@gmail.com>
2026-03-10 16:24:27 +0300

    fix P4

--- khvaevskiy.daniil/P4/func.cpp
+++ khvaevskiy.daniil/P4/func.cpp
@@ -1,6 +1,22 @@
 #include "func.h"
 
-size_t khvaevskiy::exc_snd(const char* str1, size_t len1, const char* str2, size_t len2, char* result)
+size_t khvaevskiy::read_line(char* buffer, size_t max_size)
+{
+  size_t len = 0;
+  char ch;
+
+  while (len < max_size - 1 && std::cin.get(ch) && ch != '\n')
+  {
+    buffer[len++] = ch;
+  }
+
+  buffer[len] = '\0';
+  return len;
+}
+
+size_t khvaevskiy::exc_snd(const char* str1, size_t len1,
+                           const char* str2, size_t len2,
+                           char* result)
 {
   size_t result_len = 0;
 
@@ -27,7 +43,7 @@ size_t khvaevskiy::exc_snd(const char* str1, size_t len1, const char* str2, size
 size_t khvaevskiy::rmv_vow(const char* str, size_t len,
                            char* result)
 {
-  const char* vowels = "aeiouAEIOU";
+  constexpr const char* vowels = "aeiouAEIOU";
   size_t result_len = 0;
 
   for (size_t i = 0; i < len; ++i)
--- khvaevskiy.daniil/P4/main.cpp
+++ khvaevskiy.daniil/P4/main.cpp
@@ -5,33 +5,18 @@
 int main()
 {
   const size_t max_size = 1024;
-  char* input = new (std::nothrow) char[max_size];
+  char* input = new char[max_size];
 
-  if (!input)
-  {
-    std::cerr << "Error: cannot allocate memory for input\n";
-    return 1;
-  }
+  size_t len = khvaevskiy::read_line(input, max_size);
 
-  std::cin.getline(input, max_size);
-
-  if (std::cin.fail() || std::cin.eof() || std::strlen(input) == 0)
+  if (len == 0)
   {
     std::cerr << "Error: no input provided\n";
     delete[] input;
     return 1;
   }
 
-  size_t len = std::strlen(input);
-
-  char* result = new (std::nothrow) char[max_size];
-
-  if (!result)
-  {
-    std::cerr << "Error: cannot allocate memory for result\n";
-    delete[] input;
-    return 1;
-  }
+  char* result = new char[max_size];
 
   size_t res_len = khvaevskiy::exc_snd(input, len, "abc", 3, result);
   result[res_len] = '\0';
